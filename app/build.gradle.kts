6c12d3b3a42bdb8c392e0a5fc6e575ab2de2118f
abubakir <usenbaevabubakir@gmail.com>
2023-10-24 17:38:34 +0600

    end

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,11 +66,6 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
 
-    //Room
-//    val room_version = "2.5.2"
-//    implementation("androidx.room:room-runtime:$room_version")
-//    ksp("androidx.room:room-compiler:$room_version")
-
 
     //Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
